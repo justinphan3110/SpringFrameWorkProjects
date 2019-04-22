ef1fee5627af5e9cdda3e223b5ae4788cd8d7df7
Long Phan <lnp26@case.edu>
2019-04-21 21:21:57 -0400

    updated with cargo:run

---
+++ eclipse-SpringFramework-workspace/to-do-list/src/main/java/todolist/controller/DemoController.java
@@ -0,0 +1,17 @@
+package todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	
+	// http://localhost:8080/to-do-list/hello
+	@ResponseBody
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello"; 
+		
+	}
+}
